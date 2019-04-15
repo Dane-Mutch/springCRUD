cb1c7a50b88cf3467fd6305fab0d880f008e7bcd
Dane <dane_mutch-92@hotmail.com>
2019-04-15 12:12:57 +0100

    controller added

---
+++ src/main/java/Controllers/AccountController.java
@@ -0,0 +1,36 @@
+package Controllers;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class AccountController {
+
+    @RequestMapping(value = "/accounts/")
+    public String getAllAccounts() {
+        return null;
+    }
+
+    @RequestMapping(value = "/accounts/{id}")
+    public String getAccount(@PathVariable long id) {
+        return null;
+    }
+
+    @RequestMapping(value = "accounts/{id}")
+    @ResponseBody
+    public String updateAccount(@PathVariable long id) {
+        return null;
+    }
+
+    @RequestMapping(value = "accounts/{id}")
+    @ResponseBody
+    public String deleteAccount(@PathVariable long id) {
+        return null;
+    }
+
+    @RequestMapping(value = "accounts/")
+    @ResponseBody
+    public String createAccount(@PathVariable long id) {
+        return null;
+    }
+
+}
